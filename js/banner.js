57f945ab07c3d04033a1e954b407b98e9c7ec670
Daniela Agudelo <daniela13agudelo@hotmail.com>
2022-02-28 18:57:24 -0500

    Start of the banner, moving images

---
+++ js/banner.js
@@ -0,0 +1,29 @@
+const banner = document.querySelector(".banner");
+
+const image1 = "./data/images/BannerStranger.jpg";
+const image2 = "./data/images/Image2.jpg";
+const image3 = "./data/images/Image3.jpg";
+
+let steps = 0;
+
+const changeBanner = () => {
+    console.log(steps);
+    switch (steps) {
+        case 0:
+            banner.src = image2;
+            steps++;
+            break;
+        case 1:
+            banner.src = image3;
+            steps++;
+
+            break;
+        case 2:
+            banner.src = image1;
+            steps = 0;
+
+            break;
+    }
+};
+
+setInterval(changeBanner(), 100);
